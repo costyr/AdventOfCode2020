d71d3dddafaa54fd30757dca67e6c064ae2b2251
Costi Rada <costi.rada@caphyon.com>
2020-12-08 09:32:39 +0200

    Day 8 solution

---
+++ Day8.js
@@ -0,0 +1,68 @@
+const util = require('./Util.js');
+
+function ParseProgram(aTotal, aElem) 
+{
+  let rawInst = aElem.split(' ');
+
+  aTotal.push({ inst: rawInst[0], param: parseInt(rawInst[1]) });
+
+  return aTotal;
+}
+
+function RunProgram(aInst) {
+  
+  let i = 0;
+  let acc = 0;
+  let instMap = [];
+  while (i < aInst.length) 
+  {
+    if (instMap[i] !== undefined)
+      return { acc: acc, exitCode: -1 };
+    else
+      instMap[i] = 1;
+
+    if (aInst[i].inst == "acc") {
+      acc += aInst[i].param;
+      i++;
+    }
+    else if (aInst[i].inst == "jmp")
+      i += aInst[i].param;
+    else if (aInst[i].inst == "nop")
+      i++;
+  }
+
+  return { acc: acc, exitCode: 0 };
+}
+
+function RepairProgram(aInst) 
+{
+  for (let i = 0; i < aInst.length; i++) {
+    if (aInst[i].inst == "jmp") {
+      aInst[i].inst = "nop";
+
+      let ret = RunProgram(aInst);
+
+      if (ret.exitCode != -1) 
+        return ret.acc;
+      else
+        aInst[i].inst = "jmp";
+    }
+    else if (aInst[i].inst == "nop") {
+      aInst[i].inst = "jmp";
+
+      let ret = RunProgram(aInst);
+      if (ret.exitCode != -1) 
+        return ret.acc;
+      else
+        aInst[i].inst = "nop";
+    }
+  }
+
+  return 0;
+}
+
+let instructions = [];
+util.ReduceInput('./Day8Input.txt', ParseProgram, instructions, '\r\n');
+
+console.log(RunProgram(instructions).acc);
+console.log(RepairProgram(instructions));
