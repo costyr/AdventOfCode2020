b5fa4b9751c5ea561d025b6765ec2ed8418ba87f
Costi Rada <costi.rada@caphyon.com>
2020-12-15 15:23:00 +0200

    Code refactoring

--- Day15.js
+++ Day15.js
@@ -1,52 +1,51 @@
 const util = require('./Util.js');
 
-function FindNumber(aNumberMap, aStartNumber, aStopTurn) {
-  let startTurn = util.ComputeMapSize(aNumberMap) + 1;
+function FindNumber(aNumberMap, aStopTurn) {
+  let startTurn = aNumberMap.size + 1;
   let i = startTurn;
-  let lastNumber = aStartNumber;
-  while (true) {
+  let lastNumber = 0;
 
-    i++;
-
-    if ((aNumberMap[lastNumber] === undefined) || (i == startTurn)) {
-      aNumberMap[lastNumber] = { last: i - 1, prev: i - 1 };
-
-      lastNumber = 0;
-      if (aNumberMap[lastNumber] === undefined)
-        aNumberMap[lastNumber] = { last: i, prev: i };
-      else {
-        aNumberMap[lastNumber].prev = aNumberMap[lastNumber].last;
-        aNumberMap[lastNumber].last = i;
-      }
-    }
-    else {
-      lastNumber = (aNumberMap[lastNumber].last - aNumberMap[lastNumber].prev);
-
-      if (aNumberMap[lastNumber] !== undefined) {
-        aNumberMap[lastNumber].prev = aNumberMap[lastNumber].last;
-        aNumberMap[lastNumber].last = i;
-      }
-    }
-
-    if (i == aStopTurn)
-      return lastNumber;
+  let last = aNumberMap.get(lastNumber);
+  if (last == undefined)
+    aNumberMap.set(lastNumber, [i - 1, i - 1]);
+  else {
+    last[0] = last[1];
+    last[1] = i;
   }
+
+  while (i < aStopTurn) {
+    i++;
+    last = aNumberMap.get(lastNumber);
+    if (last == undefined) {
+      aNumberMap.set(lastNumber, [i - 1, i - 1]);
+      lastNumber = 0;
+    }
+    else
+      lastNumber = last[1] - last[0];
+
+    last = aNumberMap.get(lastNumber);
+    if (last != undefined) {
+      last[0] = last[1];
+      last[1] = i;
+    }
+  }
+
+  return lastNumber;
 }
 
 function ParseInput(aStartNumbers, aNumberMap) {
-  let numbers = aStartNumbers.split(",");
-  for (let i = 0; i < numbers.length - 1; i++)
-    aNumberMap[numbers[i]] = { last: i + 1, prev: i + 1 };
-  return numbers[numbers.length - 1];
+  aStartNumbers.split(",").reduce((aTotal, aElem, aIndex) => {
+    aTotal.set(parseInt(aElem, 10), [aIndex + 1, aIndex + 1]);
+    return aTotal;
+  }, aNumberMap);
 }
 
 const kStartNumbers = "20,9,11,0,1,2";
 
-let numberMap = [];
-let startNumber = ParseInput(kStartNumbers, numberMap);
-console.log(FindNumber(numberMap, startNumber, 2020));
-
-numberMap = [];
+let numberMap = new Map();
 ParseInput(kStartNumbers, numberMap);
-console.log("For part 2 it will take ~30 minutes to compute solution...");
-console.log(FindNumber(numberMap, startNumber, 30000000));
+console.log(FindNumber(numberMap, 2020));
+
+numberMap.clear();
+ParseInput(kStartNumbers, numberMap);
+console.log(FindNumber(numberMap, 30000000));
